dc849255314be33f56355805bdc9a8d2783bf521
krampusuiy31 <KrampusPace@proton.me>
2025-03-01 00:55:32 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -52,3 +52,5 @@ if game.PlaceId == 2788229376 then
 elseif game.PlaceId == 301549746 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solariscb.lua",true))()
 end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/p.lua",true))()
