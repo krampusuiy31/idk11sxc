bd37de2a6d12e5dd9cd0db2a7bda415e58101851
krampusuiy31 <KrampusPace@proton.me>
2025-03-01 17:46:24 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+--// games
 if game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/r.lua",true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/p.lua",true))()
@@ -7,3 +8,30 @@ elseif game.PlaceId == 301549746 then
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/r.lua",true))()
 end
+
+--// discord invite
+local HttpService = game:GetService("HttpService")
+
+local function openDiscordInvite()
+    local requestFunc = syn and syn.request or request
+    
+    if not requestFunc then return end
+    
+    requestFunc({
+        Url = "http://127.0.0.1:6463/rpc?v=1",
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Origin"] = "https://discord.com"
+        },
+        Body = HttpService:JSONEncode({
+            cmd = "INVITE_BROWSER",
+            args = {
+                code = "wdQeeb9Eej"
+            },
+            nonce = HttpService:GenerateGUID(false)
+        })
+    })
+end
+
+openDiscordInvite()
