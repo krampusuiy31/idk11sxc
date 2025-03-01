0a5666c544f284b2923519e0bda9084684e7887d
krampusuiy31 <KrampusPace@proton.me>
2025-03-01 17:14:41 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,55 +1,7 @@
-local Players = game:GetService('Players')
-local LocalPlayer = Players.LocalPlayer
-local KickFunc = LocalPlayer.Kick
-local RobloxEnv = getrenv()
-
-hookfunction(KickFunc, newcclosure(function(self, reason)
-    return task.wait(9e9)
-end))
-
-local old
-old = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-    local Name = getnamecallmethod()
-    if Name == 'Kick' or Name == 'kick' then
-        return task.wait(9e9)
-    end
-    return old(self, ...)
-end))
-
-local Detected, Kill
-for i, v in getgc(true) do
-    if typeof(v) ~= 'table' then continue end
-
-    local DetectFunc = rawget(v, "Detected")
-    local KillFunc = rawget(v, "Kill")
-
-    if typeof(DetectFunc) == "function" and not Detected then
-        Detected = DetectFunc
-        hookfunction(DetectFunc, newcclosure(function()
-            return true
-        end))
-    end
-
-    if rawget(v, "Variables") and rawget(v, "Process") and typeof(KillFunc) == "function" and not Kill then
-        Kill = KillFunc
-        hookfunction(KillFunc, newcclosure(function()
-            return true
-        end))
-    end
-end
-
-local old
-old = hookfunction(RobloxEnv.debug.info, newcclosure(function(...)
-    local Func = ...
-    if Func == Detected or Func == Kill then
-        return coroutine.yield(coroutine.running())
-    end
-    return old(...)
-end))
-
 if game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/r.lua",true))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/p.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/bs.lua",true))()
 elseif game.PlaceId == 301549746 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solariscb.lua",true))()
 else
