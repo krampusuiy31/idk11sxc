6966f93e66f28a2b5a68c0509248e31e562ed34b
krampusuiy31 <KrampusPace@proton.me>
2025-03-01 14:52:36 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -49,8 +49,7 @@ end))
 
 if game.PlaceId == 2788229376 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/r.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/p.lua",true))()
 elseif game.PlaceId == 301549746 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solariscb.lua",true))()
 end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/krampusuiy31/idk11sxc/refs/heads/main/p.lua",true))()
